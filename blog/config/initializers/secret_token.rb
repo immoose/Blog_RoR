a06e17fa50992d57ead06b465c4082d6bffbe2b0
Yifeng Li <mitchellcoffee@hotmail.com>
2013-07-16 23:18:59 +0800

    1st commit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '40248141f7fa4f6618eaa24ada127ba67eb6206fc7d7d92206710182b472f8aee9cb578653af1aa9a0e3387a59b3957f5877ca912f89c8565555bf04ccfb43ca'
---
+++ blog/vendor/cache/ruby/1.8/gems/sqlite3-1.3.7/ext/sqlite3/siteconf20130715-36187-yomwf-0.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130715-36187-h2iu13"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
